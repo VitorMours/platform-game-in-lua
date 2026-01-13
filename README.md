fa78de03dc1adeaf5100182fad986b81db734b12
Vitor Moura <jvrezendemoura@gmail.com>
2026-01-12 21:08:57 -0300

    Creating the game readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Platform game in Lua
+
+<p align="center">
+  <a href="https://go-skill-icons.vercel.app/">
+    <img
+      src="https://go-skill-icons.vercel.app/api/icons?i=fedora,lua,json&theme=dark"
+    />
+  </a>
+</p>
+
+
+Jogo eletronico desenvolvido na linguagem lua, como caso de estudo do framework LOVE2d
---
+++ src/utils/window.lua
@@ -0,0 +1,23 @@
+local windowSettings = {
+}
+
+
+-- Coloca a tela como fullscreen e ocupa tudo
+function windowSettings:setFullScreen(
+    
+)
+
+
+end 
+
+
+
+-- Pegas todas as dimensoes de tela e salvar dentro das configuracoes do pc
+function windowSettings:getFullScreenModes()
+
+
+end
+
+
+
+return windowSettings 
